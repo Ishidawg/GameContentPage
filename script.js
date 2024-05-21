6764789a69aafbf36fd1525c31831adbb80a3ded
Willian <78714158+Ishidawg@users.noreply.github.com>
2024-05-21 09:34:27 -0300

    update windows content

--- script.js
+++ script.js
@@ -131,39 +131,127 @@ const windowsTweaksContent = content.innerHTML = `
         <body>
                 <section class="tweaks-section">
                     <div class="tweaks-div">
-                        <h2>Windows general tweaks</h2>
+                        <h2>WINDOWS TWEAKS</h2>
                         <div class="tweaks-content">
                             <article>
                                 <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam odio fugit optio autem deleniti consequatur eveniet, cumque suscipit enim debitis rerum quaerat aperiam voluptas rem illo ut unde nostrum ab.
-                                Doloribus totam distinctio mollitia magni recusandae commodi dolorem, cumque dolorum iure pariatur optio maxime, eos eaque et deleniti obcaecati impedit dicta velit laboriosam neque! Rem officia excepturi animi soluta rerum?
-                                Quidem maxime voluptatum tempore quae harum, omnis similique, et odio nesciunt assumenda veritatis in possimus dicta accusamus amet commodi earum architecto minus eaque quibusdam cumque quasi ducimus! Obcaecati, tempore animi?
-                                Modi beatae eos expedita exercitationem delectus voluptate illo natus, eum omnis blanditiis odio id officia ea eius accusamus rerum vitae tempore similique hic quaerat. Quod, quis. Voluptate dolore non rem.
-                                Rem asperiores sequi tenetur eius fugiat error totam optio vero voluptates. Molestiae quo soluta hic odio aut aliquam deserunt eum? Nisi neque cupiditate, voluptatibus harum unde animi nihil pariatur asperiores.
-                                Quos minima mollitia dolore! Atque quos ab nam quasi possimus at earum officia! Velit amet possimus iste, excepturi necessitatibus hic quis rem deserunt alias nihil nobis quia quisquam ex distinctio.
-                                Perspiciatis, voluptatem obcaecati? Numquam quam velit et debitis, error laudantium eum, deserunt non deleniti porro ut aliquid ullam eveniet accusantium? Nihil obcaecati voluptatem cupiditate hic minima in dolore ex accusamus!
-                                Corrupti commodi, alias voluptate maiores praesentium sequi ipsam modi. Accusantium autem, exercitationem porro asperiores sint impedit, optio ea sapiente minus omnis officia doloremque quis molestias ad consectetur minima, blanditiis cumque!
-                                Sapiente, porro impedit. Ipsa vitae repudiandae deserunt totam in? Quo tempore excepturi voluptate fugit delectus, provident, illum explicabo culpa aspernatur accusantium recusandae dignissimos dolor minima exercitationem numquam magnam vel cupiditate?
-                                Natus architecto autem nemo ipsa officia repellat, praesentium explicabo. Recusandae, possimus? Eveniet eos fugiat ipsa accusamus expedita illum eaque dignissimos soluta veritatis ducimus dolores at consectetur quis, excepturi architecto repudiandae?
-                                Error, unde. Aut aliquam quam corrupti neque assumenda illo architecto ea placeat harum, ut voluptates, magnam vel est perspiciatis atque modi, ex temporibus molestias cum adipisci eos. Minima, ratione aperiam!
-                                Iusto molestiae officiis, ipsum repudiandae, consequuntur deserunt non vitae omnis commodi, deleniti et officia sint cumque ea! Voluptatibus incidunt enim reprehenderit fuga animi illo. Delectus tenetur porro facere laborum maiores!
-                                Neque incidunt, at minus praesentium libero placeat iusto repellat! Magnam quod tempore earum accusamus sequi. Vitae facere corporis culpa rem eius. Ipsa expedita magni voluptatem, aut iste ducimus maiores et.
-                                Sequi, sed. Veniam molestiae perferendis maxime quod totam commodi facere dolores, laboriosam tenetur eum minima dolorem modi corrupti unde veritatis. Quas aut vel consectetur dolor veniam vitae quidem delectus vero?
-                                Ratione id suscipit adipisci. Magnam expedita delectus eveniet esse minus quaerat quis architecto aut laboriosam amet earum mollitia, deleniti tenetur exercitationem a cumque omnis eos voluptatum itaque quam obcaecati consequatur.
-                                Repellendus unde similique saepe ipsam quam impedit, eligendi suscipit vero asperiores officiis cupiditate ratione temporibus animi dolor hic debitis eius iusto molestias eaque consequatur voluptates nulla? Et excepturi cupiditate mollitia.
-                                Doloremque minima officiis a consequatur, esse culpa commodi mollitia ipsum distinctio beatae harum architecto ipsa facere est magni iure ea adipisci veniam error quas. Esse ea culpa qui aperiam omnis!
-                                Quisquam maiores modi eius. Eligendi itaque sapiente quo alias. Tenetur, aperiam ratione. Tempora reiciendis magnam asperiores, quod, veritatis facilis necessitatibus iusto distinctio aliquam quaerat qui assumenda in nulla rerum aut!
-                                Minima hic velit repudiandae alias modi quod maiores sapiente quidem voluptas qui sint, fuga architecto dolores reprehenderit adipisci iure, aut libero suscipit blanditiis laboriosam obcaecati sit aspernatur. Porro, ipsam vel!
-                                A nihil soluta inventore at. Debitis saepe sunt accusantium? Adipisci cupiditate recusandae, possimus asperiores sint sapiente, quas quaerat eum animi a libero voluptas similique quae, ipsum ut iusto officiis praesentium!
+                                    Different Games have different problems, some games load your connected devices
+                                    (even the hidden ones) one by one until you find your controller input, like Sekiro,
+                                    others don't… So even with my opinion, that I completely disagree with these youtube
+                                    videos like “100+ FPS NVIDIA DRIVER!!!”... I understand that some games may
+                                    have benefits with some options tweaked, no related with fps gains, but with stability
+                                    and reduction of stutter, even crashes. Understand what is running in your pc, in
+                                    front and background...
+                                </p>
+                            </article>
+                        </div>
+                    </div>
+                    <div class="tweaks-div">
+                        <h2>DEBLOATED WINDOWS</h2>
+                        <div class="tweaks-content">
+                            <article>
+                                <p>
+                                    This is the first step to get a better experience with gaming on windows and general. I
+                                    will not show you how to do that, cuz the way that I do was: look how and what those
+                                    automatic debloat windows tools do behind the hood and do it manually. You don't
+                                    need to do it, so just google it and get into some repos (Github, Gitlab, Gitbucket)
+                                    and look for scripts that do those things for you.
                                 </p>
                                 <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam odio fugit optio autem deleniti consequatur eveniet, cumque suscipit enim debitis rerum quaerat aperiam voluptas rem illo ut unde nostrum ab.
+                                    I recommend looking into <a href="https://privacy.sexy/" target="_blank">Privacy Sexy</a> and other tools to help
+                                    you with it.
                                 </p> 
+                            </article>
+                        </div>
+                    </div>
+                    <div class="tweaks-div">
+                        <h2>RASMAN</h2>
+                        <div class="tweaks-content">
+                            <article>
+                                <p>
+                                    What is RasMan? It’s a Remote Access Connection Manager that has dependencies
+                                    on Secure Socket Tunneling Protocol Service. Okay… But what's the catch here? If
+                                    you don't use a remote connection, please, disable this service. Wait, what?
+                                    Disable? Why? In my experiences with Red Dead Redemption 2 benchmarks, in the
+                                    past, when I had my GTX 1660 SUPER, this service running in the background
+                                    (even though I don't use a remote connection on my gaming machine) causes
+                                    stutter, when disabled, don't… So IF you don't use a remote connection, you are
+                                    safe to disable this.
+                                </p>
+                                <p>
+                                    The default behavior of those two services is: default. But on my computer, for some
+                                    reason, even though I don't use a remote connection and leave it on manual, it got
+                                    running anyways, so again, if you don't use remote connection, disable this.
+                                </p> 
+                            </article>
+                        </div>
+                    </div>
+                    <div class="tweaks-div">
+                        <h2>HID - HUMAN INPUT DEVICES</h2>
+                        <div class="tweaks-content">
+                            <article>
+                                <p>
+                                    What is HID? Is all of your devices connected into USB (general) ports in your pc.
+                                    Some games like Sekiro and the good old F.E.A.R Franchise, have problems with it.
+                                    Sekiro could not recognize your controller if you have many HIDs hidden in your
+                                    device manager, then to solve this you need to uninstall all those that are hidden…
+                                    F.E.A.R. otherwise, I got a horrible performance with alot of stutter if you have double
+                                    HID with the same pointer. To prevent and/or solve this kinda problem, I recommendthat you download USB Device Tree Viewer, and disable all that is in gray color, and disable many of the “HID-compliant vendor-defined device” and “HID-compliant consumer control device”. This will not impact your devices functionalities, maybe you get a message on windows new UI, like “not possible to load”, but actually those are loaded and works fine.
+                                </p>
+                            </article>
+                        </div>
+                    </div>
+                    <div class="tweaks-div">
+                        <h2>SOME TOOLS AND FINAL RECOMMENDATIONS</h2>
+                        <div class="tweaks-content">
+                            <article>
+                                <p>
+                                    Finally, I will recommend some websites, tools and talk about some settings that may help specific games.
+                                </p>
+                                <p>
+                                    <strong>General:</strong> I can recommend to most games, disable all in game vsync and
+                                    frame-limiter (event that I know that built-in has lesses input lag) and enable in yours
+                                    VGA driver, like VSYNC through NVCP (Nvidia Control Panel) or NI (Nvidia
+                                    Inspector) and FRAME-LIMITER in RTSS (RivaTuner Statistic Server), this will
+                                    probably give you experience more stable than the built-in with minor input lag
+                                    increase.
+                                    Inside these tools, in programs options, or per game settings, change the GPU
+                                    preferences to Prefer Maximum Performance (don't do it globally, cuz there is no
+                                    need to run you VGA at maximum clock on desktop), look through P-States and
+                                    disable it (again in per game settings) and all of power settings, so when you run
+                                    your games, you certified that it runs your gpu without driver power limit setting, of
+                                    course there windows p-states and dwm, but I recommend do mess with it, cuz it's
+                                    no gain in performance, just waste of power tbh.
+                                </p>
+                                <p>
+                                    I recommend looking into DXVK or VKD3D if you have problems running a specific
+                                    game on directX, like GTA IV for example, that has lots of benefits running in Vulkan
+                                    (DXVK) instead of DirectX.
+                                </p>
+                                <p>
+                                    If you have frame-pacing problems even with all tweaks above, you can try SpecialK
+                                    software, but be aware that games with anti-cheat probably will ban you, cuz its a dll
+                                    injection.
+                                </p>
+                                <p>
+                                    Search by sync options that may give you a better experience, sometimes you play
+                                    on your monitor at native resolution and have GPU around 60%, then you could give
+                                    a try on Scanline Sync, using RTSS. What this do? Give you the ability to move the
+                                    tearing line around your screen, so you could just drop down until its completely off
+                                    your screen, then you’ll get a smooth and no lag experience.
+                                    <strong>obs: this works great (to me) only when I run the game on my monitor native
+                                    resolution with my GPU load below 70%.</strong>
+                                </p>
+                                <h3>Softwares, tools and websites</h3>
                                 <ul>
-                                    <li>ITEM 1</li>
-                                    <li>ITEM 2</li>
-                                    <li>ITEM 3</li>
-                                    <li>ITEM 4</li>
+                                    <li>Nvidia Control Panel (AMD ALTERNATIVE)</li>
+                                    <li>Nvidia Inspector</li>
+                                    <li>USB Device Tree Viewer</li>
+                                    <li>DXVK</li>
+                                    <li>VKD3D</li>
+                                    <li>RTSS</li>
+                                    <li>SpecialK</li>
+                                    <li>PC Gaming Wiki</li>
                                 </ul>
                             </article>
                         </div>
